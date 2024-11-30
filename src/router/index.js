930d95ec2f9fc5b2917289c77cb5cdbbce41f947
Kimsreng28 <kimsrenghai@gmail.com>
2024-11-30 13:20:37 +0700

    task1 and task2 completed

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,23 @@ import { createRouter, createWebHistory } from "vue-router";
 
 const router = createRouter({
   history: createWebHistory(),
-  routes: [],
+  routes: [
+    {
+      path: "/",
+      name: "Home",
+      component: () => import("../views/HomeView.vue"),
+    },
+    // {
+    //   path: "/categories/:categoryId",
+    //   name: "category",
+    //   component: () => import("../views/CategoryPage.vue"),
+    // },
+    // {
+    //   path: "products/:productId",
+    //   name: "product",
+    //   component: () => import("../views/ProductDetailPage.vue"),
+    // },
+  ],
 });
 
 export default router;
